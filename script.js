0987e82276bc1912cf23df4263bbd6dfde2fc03f
Jeof Petty <jeofpetty@icloud.com>
2022-06-14 23:12:33 -0400

    obj enhance exercise

---
+++ script.js
@@ -0,0 +1,62 @@
+// In this exercise, you’ll refactor some ES5 code into ES2015. Write your code in the sections with a
+// comment to “Write an ES2015 Version”.
+
+// function createInstructor(firstName, lastName){
+//     return {
+//       firstName: firstName,
+//       lastName: lastName
+//     }
+//   }
+
+const createInstructor = (firstName, lastName) => ({firstName, lastName});
+
+// var favoriteNumber = 42;
+
+// var instructor = {
+//   firstName: "Colt"
+// }
+
+// instructor[favoriteNumber] = "That is my favorite!"
+
+let favoriteNumber = 42;
+const instructor = {
+    firstName : 'Colt',
+    favoriteNumber
+}
+
+// var instructor = {
+//     firstName: "Colt",
+//     sayHi: function(){
+//       return "Hi!";
+//     },
+//     sayBye: function(){
+//       return this.firstName + " says bye!";
+//     }
+//   }
+
+const instructorGaget = {
+    firstName : 'Colt',
+    sayHi(){
+        return 'Hi';
+    },
+    sayBye(){
+        return this.firstName + ' says bye!';
+    }
+}
+
+// Write a function which generates an animal object. The function should accepts 3 arguments:
+
+//      species: the species of animal (‘cat’, ‘dog’)
+//      verb: a string used to name a function (‘bark’, ‘bleet’)
+//      noise: a string to be printed when above function is called (‘woof’, ‘baaa’)
+// Use one or more of the object enhancements we’ve covered.
+
+const createAnimal = (species, verb, noise) => {
+    return {
+        species,
+        [verb](){
+            return noise;
+        }
+    }
+}
+
